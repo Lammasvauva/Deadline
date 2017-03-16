61e08b219abf0312b890390a82db538cc6a221d0
Tomi <tomi.vahde@gmail.com>
2017-03-16 11:08:08 +0200

    Created Develop branch

--- app/src/main/java/d4/deadline/MainActivity.java
+++ app/src/main/java/d4/deadline/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //This is a commit from computer C
+        //This is now develop branch
     }
 }
