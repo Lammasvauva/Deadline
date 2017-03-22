8256638751ea55413107b82af99cf32bd17bcaed
Tomi <Tomi.Vahde@gmail.com>
2017-03-22 22:38:47 +0200

    New buttons work, still working on fragments

---
+++ app/src/main/java/d4/deadline/MainMenuNavigation.java
@@ -0,0 +1,62 @@
+package d4.deadline;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import layout.FirstFragment;
+import layout.SecondFragment;
+import layout.ThirdFragment;
+
+public class MainMenuNavigation extends AppCompatActivity
+{
+
+    private TextView mTextMessage;
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+
+                    return true;
+            }
+
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu_navigation);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+}
