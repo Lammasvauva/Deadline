45adb03a9818b2b74a5abbb74485f21c24d07360
Tomi <tomi.vahde@gmail.com>
2017-03-23 11:36:21 +0200

    Testing fragments

--- app/src/main/java/d4/deadline/MainMenuNavigation.java
+++ app/src/main/java/d4/deadline/MainMenuNavigation.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -28,14 +30,24 @@ public class MainMenuNavigation extends AppCompatActivity
 
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    //tässä pitäisi näkyä fragment
                     mTextMessage.setText(R.string.title_home);
+
+                    /*
+                    Fragment eka = new FirstFragment();
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                    transaction.replace(R.id.container, eka).commit();
+*/
+
+
+
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
-
                     return true;
             }
 
@@ -46,11 +58,6 @@ public class MainMenuNavigation extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
-
-
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu_navigation);
