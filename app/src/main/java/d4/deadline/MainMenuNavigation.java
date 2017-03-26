794a925cdaf87389f8a7376b06c0ce02a7589ebf
Tomi <Tomi.Vahde@gmail.com>
2017-03-26 19:05:58 +0300

    Navigation bar works with fragments.

--- app/src/main/java/d4/deadline/MainMenuNavigation.java
+++ app/src/main/java/d4/deadline/MainMenuNavigation.java
@@ -12,54 +12,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import layout.BlankFragment;
 import layout.FirstFragment;
 import layout.SecondFragment;
 import layout.ThirdFragment;
 
 public class MainMenuNavigation extends AppCompatActivity implements FirstFragment.OnFragmentInteractionListener,
-SecondFragment.OnFragmentInteractionListener, ThirdFragment.OnFragmentInteractionListener{
+SecondFragment.OnFragmentInteractionListener, ThirdFragment.OnFragmentInteractionListener, BlankFragment.OnFragmentInteractionListener{
 
     private TextView mTextMessage;
 
-    //fragments
-    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-   // fragmentTransaction.replace();
-
-
-
+    //FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-
         @Override
+        //Tämä kuuntelee navigation barin klikkauksia ja vaihtaa fragmentit
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
             Fragment fragment = null;
 
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.navigation_whiteboard:
                     fragment = FirstFragment.newInstance();
-
-                    //tässä pitäisi näkyä fragment
-                   // mTextMessage.setText(R.strin  g.title_home);
-                    //Fragment eka = new FirstFragment();
-                   // FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                    //transaction.replace(R.id.container, eka).commit();
+                    break;
+                case R.id.navigation_home:
+                    fragment = SecondFragment.newInstance();
                     break;
                 case R.id.navigation_dashboard:
-                    fragment = SecondFragment.newInstance();
-                    //mTextMessage.setText(R.string.title_dashboard);
-                    break;
-                case R.id.navigation_notifications:
                     fragment = ThirdFragment.newInstance();
-                   // mTextMessage.setText(R.string.title_notifications);
                     break;
             }
             if(fragment != null)
             {
                 FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.container, fragment);
+                fragmentTransaction.replace(R.id.fragment_container, fragment);
                 fragmentTransaction.commit();
             }
 
@@ -71,12 +58,28 @@ SecondFragment.OnFragmentInteractionListener, ThirdFragment.OnFragmentInteractio
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu_navigation);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //set default fragment when opening app
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, FirstFragment.newInstance());
+        fragmentTransaction.commit();
+
+
+        //region set default highlighted navigation bar item
+
+        //endregion
+
+
+
+
+
     }
 
 }
