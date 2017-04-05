857c460093ca186c0bd3a9b38784648a1f47c86a
Tomi <Tomi.Vahde@gmail.com>
2017-04-05 14:19:35 +0300

    Adding new notes works, starting work on popup window

--- app/src/main/java/layout/FirstFragment.java
+++ app/src/main/java/layout/FirstFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,8 +15,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import d4.deadline.MainMenuNavigation;
 import d4.deadline.R;
 
 /**
@@ -30,18 +37,55 @@ import d4.deadline.R;
 public class FirstFragment extends Fragment {
 
     private FirstFragment.OnFragmentInteractionListener listener;
-
     public static FirstFragment newInstance() {
         return new FirstFragment();
     }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.fragment_first, container, false);
+            final View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+
             Button clickButton = (Button)view.findViewById(R.id.addbutton_whiteboard);
-            clickButton.setOnClickListener(new View.OnClickListener() {
+            clickButton.setOnClickListener(new View.OnClickListener()
+            {
+
+
+                EditText et=(EditText)view.findViewById(R.id.addTextLine1);
+                EditText et2=(EditText)view.findViewById(R.id.addTextLine2);
+
                 @Override
-                public void onClick(View v) {
+                //Put button functionality here
+                public void onClick(View v)
+                {
+                    //find LinearLayout of mainActivity for some reason
+                    //Works only here, not in it's own method, no idea why
+                    LinearLayout myLayout = (LinearLayout) view.findViewById(R.id.linearlayout);
+
+                    //region Create new Note
+                    TextView a = new TextView(view.getContext());
+
+                    a.setText("Tämä on luotu juuri nyt");
+                    a.setHeight(150);
+                    a.setGravity(Gravity.CENTER);
+                   // a.setBackground();
+
+
+                    myLayout.addView(a);
+
+                    //Bring out text field
+                    //et.setVisibility(View.VISIBLE);
+                    et2.setVisibility(View.VISIBLE);
+                    CreateWhiteboardNote();
+                    SetNoteText();
+
+                    //endregion
+
+
+
+
+
+
                     Toast.makeText(getActivity(), "Fragment Button Click", Toast.LENGTH_LONG).show();
                     onButtonPressed("Fragment Button Click");
                 }
@@ -72,9 +116,23 @@ public class FirstFragment extends Fragment {
             listener = null;
         }
 
-
     public interface OnFragmentInteractionListener {
             public void onFragmentInteraction(String uri);
         }
+
+
+    public  void CreateWhiteboardNote()
+        {
+
+
+
+        }
+
+    public  void SetNoteText()
+    {
+        TextView t = (TextView) getView().findViewById(R.id.textView8);
+        t.setText("Panoin nappia");
+
+    }
 
     }
